d530e7a622a08c2ae61676af27b9b652fc9bc2ef
capitan america <Phuritry2222@gmail.com>
2026-05-28 15:17:10 +0700

    by phuri

--- main.java
+++ main.java
@@ -1,5 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 public class Main extends Application{
     public static void main(String[] args) {
-        
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        // TODO Auto-generated method stub
+        stage.show();
     }
 }
