b5ce619d01fae64758d8526137a51675a1473888
voeunlyna-cyber <voeunlyna@gmail.com>
2026-05-29 13:38:30 +0700

    update

--- main.java
+++ main.java
@@ -6,8 +6,9 @@ import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-    public static void main(String[] args) {
-        launch();
+    public static void main(String[] args){
+
+       launch();
     }
 
     @Override
